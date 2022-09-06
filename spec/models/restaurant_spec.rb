c1096c402b7cba81fb1ac47779f9c60b578b5a6d
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-09-06 08:44:56 -0300

    refactor(restaurant): add shoulda matchers and refactor tests

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
+  let(:restaurant_) {create(:restaurant)}
+  let(:products) {create_list( :product, rand(5), restaurant: restaurant_)}
+
+  context "Restaurant have connection with products" do
+    it { should have_many(:products) }
+  end
+
   context "testing total of products" do
     it "return total of products for a specific restaurant" do
-      restaurant_ = create(:restaurant)
-      products = create( :product, restaurant: restaurant_)
-
       expect(restaurant_.total_of_products).to eq restaurant_.products.length
     end
   end
